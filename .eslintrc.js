fcc39266c87a0c937cb37af01839e46ee65062e4
Tom Wieland <tom.wieland@gmail.com>
2016-12-11 18:02:13 +0000

    added eslint and resolved the issues

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "semi": 0,
+    "max-len": 0,
+    "no-underscore-dangle": 0,
+    "no-param-reassign": 0,
+  }
+};
