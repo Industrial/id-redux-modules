c00be72465eb9e8909376111dd806026a625821e
Tom Wieland <tom.wieland@gmail.com>
2016-12-11 22:47:52 +0000

    converted to getter/setter syntax

--- example/application/index.js
+++ example/application/index.js
@@ -1,15 +1,8 @@
-// Plain and simple.
-
-const reducers = require('./reducers')
-const routes = require('./routes')
-
-const blogModule = require('./modules/blog')
-
 module.exports = {
   name: 'Application',
 
-  modules: [ blogModule ],
+  modules: [ require('./modules/blog') ],
 
-  reducers,
-  routes,
+  reducers: require('./reducers'),
+  routes: require('./routes'),
 }
